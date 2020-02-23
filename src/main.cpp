b7e9ffd26a3c50e54ee9b5f36f8d50276bf2d8b9
Kalle <kalllebracht@gmx.de>
2020-02-23 16:47:26 +0100

    add comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,26 @@
-#include <Arduino.h>
-#include <PID_v1.h>
-#include <Wire.h>
-#include <Servo.h>
-#include "/home/kalle/Documents/PlatformIO/Projects/pidgonelsteuerung/lib/MPU6050_tockn.h"
+#include <Arduino.h>  //Arduino Framework for basic commands
+#include <PID_v1.h>   //PID libarry 
+#include <Wire.h>     //I2C Bus libarry
+#include <Servo.h>    //Servo libarry(PWM Signal)
+#include "/home/kalle/Documents/PlatformIO/Projects/pidgonelsteuerung/lib/MPU6050_tockn.h"  //Libarry MPU6050 reading via I2C(Wire)
 
-#define MPU6050_ADRESS     0x68
+#define MPU6050_ADRESS     0x68   //Adress for MPPU6050
+       
 
-
-double serialinput = 0;
-
-double Setpoint ; 
-double Input; 
-double Output ;
+double Setpoint = 0;  //Point for PID
+double Input    = 0;  //Value from MPU6050 for PID
+double Output   = 0;  //Output for Servo from PID
 
 double Outputreadyforservo;
 
 double Kp=100, Ki=100, Kd=0; 
 
-PID myPID(&serialinput, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
+PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 
 MPU6050 mpu6050(Wire);
 
 Servo tester;
+
 
 void setup() {
   tester.attach(2);
@@ -32,7 +31,6 @@ void setup() {
   mpu6050.begin();
   mpu6050.calcGyroOffsets(true);
 
-  Setpoint = 0;
   //Turn the PID on
   myPID.SetMode(AUTOMATIC);
   //Adjust PID values
@@ -41,19 +39,23 @@ void setup() {
 }
 
 void loop() {
-
+  //Update ther MPU6050
   mpu6050.update();
   
-  serialinput = mpu6050.getAccAngleX();
-
+  //Made all ready for the servo
+  Input = mpu6050.getAccAngleX();
   Outputreadyforservo = map(Output, 0, 255, 0, 35);
 
+  //write to servo
   tester.write(Outputreadyforservo+ 90);
 
+  //Compute the PID
   myPID.Compute();
   
+
+  //Print the infos to serial
   Serial.print("Input: ");
-  Serial.println(serialinput);
+  Serial.println(Input);
   Serial.print("Output: ");
   Serial.println(Outputreadyforservo);
   Serial.print("Setpoint: ");  
