855dd6e838dc33e51d2d171222cc8678051575bb
Kalle <kalllebracht@gmx.de>
2020-02-27 14:33:06 +0100

    add calculate stuff

--- src/calculateengine.cpp
+++ src/calculateengine.cpp
@@ -1,12 +1,28 @@
 #include "calculateengine.h"
 
-void calculate::firstdefine(int *servo1, int *servo2, int *servo3, int *servo4, pidvalue xpid, pidvalue ypid){
+void calculate::offset(int fromservo1, int fromservo2, int fromservo3, int fromservo4){
+    servo1offset1 = fromservo1;
+    servo1offset2 = fromservo2;
+    servo1offset3 = fromservo3;
+    servo1offset4 = fromservo4;
+}
+
+void calculate::run(int *servo1, int *servo2, int *servo3, int *servo4, pidvar xpid, pidvar ypid){
     if(xpid.direction){
-        *servo1 = xpid.value;
-        *servo3 = -(xpid.value);
+        *servo1 =   xpid.value  + servo1offset1;
+        *servo3 = -(xpid.value) + servo1offset3;
     }
     else{
-        *servo3 = -(xpid.value);
-        *servo1 = xpid.value;
+        *servo3 = -(xpid.value) + servo1offset3;
+        *servo1 =   xpid.value  + servo1offset1;
+    }
+
+    if(ypid.direction){
+        *servo2 =   ypid.value  + servo1offset2;
+        *servo4 = -(ypid.value) + servo1offset4;
+    }
+    else{
+        *servo4 = -(ypid.value) + servo1offset4;
+        *servo2 =   ypid.value  + servo1offset2;
     }
 }
--- src/calculateengine.h
+++ src/calculateengine.h
@@ -1,15 +1,21 @@
 #ifndef CALCULATE_H
 #define CALCULATE_H
 
-class pidvalue{
-public:
+struct pidvar{
     int value;
     bool direction;
 };
 
 class calculate{
 public:
-    void firstdefine(int* servo1, int* servo2, int* servo3, int* servo4, pidvalue xpid, pidvalue ypid);
+    void run(int* servo1, int* servo2, int* servo3, int* servo4, pidvar  xpid, pidvar ypid);
+    void offset(int fromservo1, int fromservo2, int fromservo3, int fromservo4);
+
+private:
+    int servo1offset1 = 0;
+    int servo1offset2 = 0;
+    int servo1offset3 = 0;
+    int servo1offset4 = 0;
 };
 
 #endif // CALCULATE_H
